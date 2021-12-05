e11e1d3d23140a8c00ff9e60a9dcf8401dd0cc01
unknown <cleo_dima@mail.ru>
2021-12-05 18:56:26 +0300

    add hw10

---
+++ hw10/object=/1.js
@@ -0,0 +1,61 @@
+//Заваданя 2
+
+
+// let user = {
+//      name: 'roma',
+//      surname: ' holub',
+//      address: 'skripnika',
+
+// }
+
+// let copyobject =  function(user){
+//      let copyObj = {};
+
+//      for(let key in user){
+//           copyObj[key] = user[key];
+                                                           
+//      }
+
+//      return copyObj;
+// }
+// console.log(copyobject(user))
+
+
+
+
+
+
+
+
+
+
+// Завдання 1 
+
+// let console2 = {
+     
+// }
+// for(let key in console){
+//      console2[key] = console[key]
+// }
+// console2.log('1231')
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Завдання 3 
+
+
+// let prop = {
+//      name:'roma',
+//      age: '18',
+// }
+// console.log(Object.assign({}, prop))
